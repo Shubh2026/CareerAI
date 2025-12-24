0ea59653f40391f6ca9213aae22a4d4273c8b4ff
Shubh <shubhrawat006@gmail.com>
2025-12-24 10:42:01 +0530

    fix: remove ensureAnonymousAuth import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { ensureAnonymousAuth } from "@/lib/firebase";
-ensureAnonymousAuth().catch(console.error);
-
 createRoot(document.getElementById("root")!).render(<App />);
